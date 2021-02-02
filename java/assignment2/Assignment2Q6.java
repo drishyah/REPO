ca71b9b98124250060caed6913b3836524cc6e93
drishyah <43957436+drishyah@users.noreply.github.com>
2021-02-02 15:07:08 +0530

    Add files via upload

---
+++ java/assignment2/Assignment2Q6.java
@@ -0,0 +1,30 @@
+abstract class Persistence {
+    abstract public String persist();
+}
+class FilePersistence extends Persistence{
+    @Override
+    public String persist() {
+    	return "FilePersistence";
+    }
+}
+class DatabasePersistence extends Persistence{
+    @Override
+    public String persist() {
+    	return "DatabasePersistence";
+    }
+}
+
+	
+}
+
+public class Assignment2Q6 {
+    public static void main(String[] args) {
+    Persistence p = new Filepersistence();
+    p.persist();
+   
+    Persistence p = new Databasepersistence();
+    p.persist();
+    
+    }
+    
+}
