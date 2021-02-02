fc2a0b7e2f98cf73baf6cf3009baa64a6b664c2d
drishyah <43957436+drishyah@users.noreply.github.com>
2021-02-02 10:14:38 +0530

    Add files via upload

---
+++ java/assignment1/Assignment1Q6.java
@@ -0,0 +1,52 @@
+import java.util.Scanner;
+class Login{
+    String userId = "Ajay",password = "password";
+    static int count =2;
+    public String loginUser(String user, String pass) {
+    	Scanner scan = new Scanner(System.in);
+    	while(count>0) {
+    		if(user.equals(userId) && pass.equals(password)) {
+    			scan.close();
+    			return "Welcome Ajay";
+    		}
+    		else {
+    			System.out.println("You have entered wrong credentials ,please enter the right credentials.");
+    			count--;
+    			user = scan.nextLine();
+    			pass = scan.nextLine();
+    		}
+    		
+    	}
+    	scan.close();
+    	System.out.println("You have entered wrong credentials 3 times");
+    	return "Contact Admin";
+    }
+
+}
+public class Assignment1Q6 {
+    public static void main(String[] args) {
+    	Login login = new Login();
+    	Scanner scan = new Scanner(System.in);
+    	String user = "",pass = "";
+    	user = scan.nextLine();
+    	pass = scan.nextLine();
+    	scan.close();
+    	System.out.println(login.loginUser(user, pass));	
+    }
+}
+  }
+}
+public class Assignment1Q6 {
+    public static void main(String[] args) {
+    	Login log=new Login();
+        Scanner sc = new Scanner();
+        String userId, password;
+        System.out.println("Enter userid"); 
+        userId=sc.nextLine();
+        System.out.println("Enter password"); 
+        password=sc.nextLine();
+
+        System.out.println(Login.loginUser(userId,password));
+  
+    }
+}
