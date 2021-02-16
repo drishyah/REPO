c1d8c9145f9370bc62ccc0c31912dc6b0fd2e30e
drishyah <43957436+drishyah@users.noreply.github.com>
2021-02-16 11:55:52 +0530

    Add files via upload

---
+++ SpringCore/Spring4and5/Annotations/src/main/java/com/drishya/Annotations/App.java
@@ -0,0 +1,14 @@
+package com.drishya.Annotations;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class App{
+    public static void main( String[] args ){
+    	ApplicationContext context = new ClassPathXmlApplicationContext("spring.xml");
+		
+    	System.out.println("All Details from details class");
+    	Details details = (Details)context.getBean("details");
+    	System.out.println(details);
+    }
+}
